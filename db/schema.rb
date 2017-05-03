16fd4491c272846050237a5aa921b530f8beab82
Christopher Choi <cdddchris@gmail.com>
2017-05-02 17:27:18 -0700

    alter schema, add relation to user and coffeebeans

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170503000545) do
+ActiveRecord::Schema.define(version: 20170503002508) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 20170503000545) do
     t.datetime "created_at", null: false
     t.datetime "best_by"
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_coffeebeans_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
