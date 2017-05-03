908fe866649248fc9aa5f951e929ad4b21117004
Christopher Choi <cdddchris@gmail.com>
2017-05-02 17:04:24 -0700

    update schema

--- db/migrate/20170502234635_create_coffeebeans.rb
+++ db/migrate/20170502234635_create_coffeebeans.rb
@@ -1,7 +1,14 @@
 class CreateCoffeebeans < ActiveRecord::Migration[5.0]
   def change
     create_table :coffeebeans do |t|
-
+      t.string :name
+      t.string :brand
+      t.string :roast_type
+      t.text :notes
+      t.integer :score
+      t.datetime :created_at
+      t.datetime :best_by
+      
       t.timestamps
     end
   end
