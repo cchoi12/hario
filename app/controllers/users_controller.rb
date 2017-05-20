eafe1d2187ce1d2e18a7cc4f6ff6168296230131
Christopher Choi <cdddchris@gmail.com>
2017-05-20 23:51:27 +0900

    fix show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,8 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!
-
-  def index
-  end
-
+  
   def show
-    @user = User.find_by_username(params[:id])
+    @user = User.find_by_user_name(params[:id])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  match '/users',   to: 'users#index',   via: 'get'
-  match '/users/:id',     to: 'users#show',       via: 'get'
+  get 'users/index'
+  match '/users',   to: 'users#show',   via: 'get'
 
   devise_for :users, :controllers => { registrations: 'registrations' }
-  devise_for :users, :path_prefix => 'd'
   resources :users, :only =>[:show]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'coffeebeans#index'
 
