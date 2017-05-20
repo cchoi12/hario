0bf0e04916cc7a0fed055b3d532f2fe25bc16d63
Christopher Choi <cdddchris@gmail.com>
2017-05-20 18:33:00 +0900

    add user controllerr

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/index'
+  match '/users',   to: 'users#index',   via: 'get'
+  match '/users',   to: 'users#show',   via: 'get'
 
   devise_for :users, :controllers => { registrations: 'registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
